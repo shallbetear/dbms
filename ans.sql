9b7779037db488208032e8090b0cfe80e8fe5bd2
shallbetear <155661349+shallbetear@users.noreply.github.com>
2024-06-06 20:48:15 +0530

    Update ans.sql

--- ans.sql
+++ ans.sql
@@ -203,3 +203,34 @@ begin
 end;
 
 select * from employee;
+
+------------------------------------------------------------------------------------------------------------------------------------
+--Product probm
+create table Products (
+    ProductID int primary key,
+    ProductName varchar(20), 
+    Price int, 
+    StockQuantity int
+    );
+
+-- Task 1: Create the view
+create view ProductDetails as
+select ProductID, ProductName, Price, StockQuantity
+from Products;
+
+-- Task 2: Drop the view if it exists
+begin
+   execute immediate 'drop view ProductDetails';
+exception
+   when others then
+      if sqlcode != -942 then
+         raise;
+      end if;
+end;
+/
+drop view ProductDetails;
+
+-- Task 3: Verify if the view exists
+select view_name
+from user_views
+where view_name = 'PRODUCTDETAILS';
