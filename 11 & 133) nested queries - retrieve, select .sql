d06c5571d3c6621ed1aa5c769f8dc9c411cf8dd9
shallbetear <155661349+shallbetear@users.noreply.github.com>
2024-06-06 20:07:56 +0530

    Create 11 & 133) nested queries - retrieve, select .sql

---
+++ 11 & 133) nested queries - retrieve, select .sql
@@ -0,0 +1,14 @@
+create table student (
+    reg_no int primary key,
+    name varchar(50),
+    dept varchar(20),
+    city varchar(20),
+    ph_no int
+);
+insert into student values (1, 'saara', 'AI', 'tirupur', 456789);
+insert into student values (2, 'laara', 'EEE', 'coimbatore', 123456);
+insert into student values (3, 'caara', 'ECE', 'coimbatore', 345789);
+insert into student values (4, 'meera', 'BME', 'erode', 124566);
+select * from student;
+
+select * from student where dept in ( select dept from student where city = 'coimbatore' );
